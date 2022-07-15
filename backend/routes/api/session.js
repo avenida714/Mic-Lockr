11b2d33c842ae3b5d29a4b81647d72d543d00a9d
Alec Venida <alecvenida@gmail.com>
2022-07-15 15:32:23 -0700

    validation login OK

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,17 +1,36 @@
 // backend/routes/api/session.js
 
 const express = require('express');
+
 const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+const { check } = require('express-validator');
+
+const { handleValidationErrors } = require('../../utils/validation');
+
+//validator/validation chain middleware
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 
 // Log in
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
