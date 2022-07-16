588730fd331c2db23478878a9f8a3bbda6024f1b
Alec Venida <alecvenida@gmail.com>
2022-07-16 16:30:56 -0700

    frontend/src/spp.js and frontend/src/index.js updated for modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,19 @@
 // frontend/src/index.js
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
 
-import './index.css';
-
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
-import { restoreCSRF, csrfFetch } from './store/csrf';
-
-import configureStore from './store';
-
-import * as sessionActions from './store/session';
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -24,27 +21,17 @@ if (process.env.NODE_ENV !== 'production') {
   window.sessionActions = sessionActions;
 }
 
-// if (process.env.NODE_ENV !== 'production') {
-//   window.store = store;
-// }
-
-// if (process.env.NODE_ENV !== 'production') {
-//   restoreCSRF();
-
-//   window.csrfFetch = csrfFetch;
-//   window.store = store;
-// }
-
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
-
 
 ReactDOM.render(
   <React.StrictMode>
