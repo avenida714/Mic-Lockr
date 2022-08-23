3a79de308704bc5d6595dc31e35844a143612a26
Alec Venida <alecvenida@gmail.com>
2022-08-23 15:45:37 -0700

    develop handleSubmit in add mic component form

--- frontend/src/components/AddMic/index.js
+++ frontend/src/components/AddMic/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+import { createMic } from '../../store/mics'
 
 
 function AddMic() {
@@ -18,6 +19,32 @@ function AddMic() {
   const [errors, setErrors] = useState([])
 
 
+
+  //force the user to sign up if they access this page
+  useEffect(() => {
+    if (!personLoggedIn) {
+      history.push('/signup')
+    }
+  }, [personLoggedIn, history])
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newMicForTheLockr = {
+      userId: personLoggedIn.id,
+      imageURL,
+      title,
+      description
+
+    }
+    await dispatch(createMic(newMicForTheLockr))
+      .then((newMic) => history.push(`/mics/${newMic.id}`))
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && errors) setErrors(data.errors)
+      })
+  }
 
 
   return (
