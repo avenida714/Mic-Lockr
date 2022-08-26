03c72aab23873a92a801917d7867698411e55290
Alec Venida <alecvenida@gmail.com>
2022-08-25 19:28:29 -0700

    form for create comment done

--- frontend/src/components/AddComment/index.js
+++ frontend/src/components/AddComment/index.js
@@ -5,18 +5,25 @@ import React, { useState } from 'react'
 function AddComment() {
 
   const [comment, setComment] = useState('')
+  const [errors, setErrors] = useState([])
 
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
 
   return (
     <>
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Comment On This Mic!</label>
       <textarea
-
+      type="textarea"
+      value={comment}
+      onChange={(e) => setComment(e.target.value)}
+      placeholder="What are your thoughts on this Microphone?"
+      required
       />
-
-
+      <button type="submit">Submit Comment</button>
     </form>
     </>
   )
