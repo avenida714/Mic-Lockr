d98439a9dafb23472bc10ae25dd55f24cdce9f7e
Alec Venida <alecvenida@gmail.com>
2022-08-27 18:56:28 -0700

    flex page looking better

--- frontend/src/components/FlexPage/index.js
+++ frontend/src/components/FlexPage/index.js
@@ -9,8 +9,8 @@ function FlexPage() {
     <div className='homeMicContainer'>
       <img className='sm7b-Flex' src={sm7b} alt="sm7b" width="100%"></img>
       <div className='centered'>
-      <h1 className='title'>Mic Lockr</h1>
-      <h3 className='catchPhrase'>Flex Your Vocal Cords</h3>
+      <h1 id='title'>mic lockr</h1>
+      <h3 id='catchPhrase'>flex your vocal cords</h3>
       </div>
     </div>
   )
