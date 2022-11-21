7940a479ca122cc25c53a8a3985a782472ce8320
Alec Venida <alecvenida@gmail.com>
2022-11-21 09:13:57 -0800

    navbar started, need to edit links

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -25,12 +25,59 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <div className="navBar-div">
-        <i className="annoyingAnchor" exact to="/"><img src={logo} height="60px" width="60px"alt="logo"/>
-        </i>
+    // <div className="navBar-div">
+    //     <i className="annoyingAnchor" exact to="/"><img src={logo} height="60px" width="60px"alt="logo"/>
+    //     </i>
 
-        {isLoaded && sessionLinks}
+    //     {isLoaded && sessionLinks}
+    // </div>
+    <>
+
+    <div id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href="/" class="logo d-flex align-items-center  me-auto me-lg-0">
+
+      {/* <img src="assets/img/logo.png" alt="">  */}
+        <i class="bi bi-camera"></i>
+        <h1>Mic-Lockr</h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="/" class="active">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li class="dropdown"><a href="#"><span>Gallery</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="gallery.html">Nature</a></li>
+              <li><a href="gallery.html">People</a></li>
+              <li><a href="gallery.html">Architecture</a></li>
+              <li><a href="gallery.html">Animals</a></li>
+              <li><a href="gallery.html">Sports</a></li>
+              <li><a href="gallery.html">Travel</a></li>
+              <li class="dropdown"><a href="#"><span>Sub Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="#">Sub Menu 1</a></li>
+                  <li><a href="#">Sub Menu 2</a></li>
+                  <li><a href="#">Sub Menu 3</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+
+      <div class="header-social-links">
+      {isLoaded && sessionLinks}
+      </div>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+     </div>
     </div>
+    </>
 
   );
 }
