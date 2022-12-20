3fe16b5d7aa2b9f1264dfd227203d9b6a7da7a83
Alec Venida <alecvenida@gmail.com>
2022-12-19 18:15:11 -0800

    comments css, cleanup

--- frontend/src/components/AddComment/index.js
+++ frontend/src/components/AddComment/index.js
@@ -6,6 +6,9 @@ import { useParams } from 'react-router-dom';
 
 import { createCommentThunk } from '../../store/comments';
 
+import './AddComment.css'
+
+
 function AddComment() {
 
   const [comment, setComment] = useState('')
@@ -48,15 +51,18 @@ function AddComment() {
     <ul>
         {errors.map((error, index) => <li key={index}>{error}</li>)}
       </ul>
-      <label>Comment On This Mic!</label>
-      <textarea
+      <div className='comment-container'>
+         <label>Comment On This Mic!</label>
+      <textarea className='comment-text-area'
       type="textarea"
       value={comment}
       onChange={(e) => setComment(e.target.value)}
       placeholder="What are your thoughts on this Microphone?"
       required
       />
-      <button type="submit">Submit Comment</button>
+      <button className='submit-button' type="submit">Submit Comment</button>
+      </div>
+
     </form>
     </>
   )
